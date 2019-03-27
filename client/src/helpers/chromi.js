a23dd19528943bb522f5f87404dc6facfbe0e1a9
Nyan Of The Moon <nyanofthemoon@gmail.com>
2019-03-27 14:35:27 -0400

    a

---
+++ client/src/helpers/chromi.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const successCallback = payload => ({ payload });
+const errorCallback = error => ({ error });
+
+const config = {
+  crossdomain: true,
+  withCredentials: true,
+};
+
+const login = (username, password) => axios.post(`${window.CLIN.apiBaseUrl}/auth`, {
+  username,
+  password,
+}, config)
+  .then(successCallback)
+  .catch(errorCallback);
+
+const logout = () => axios.delete(`${window.CLIN.apiBaseUrl}/auth`, config)
+  .then(successCallback)
+  .catch(errorCallback);
+
+export class ApiError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export default {
+  login,
+  logout,
+};
