9f07a38aa8707d7f78082c5a633c2fcf04db4e13
Nyan Of The Moon <nyanofthemoon@gmail.com>
2019-07-02 14:07:06 -0400

    ui updates

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,31 +3,80 @@ import { connect } from 'react-redux';
 import {
   Layout, Row, Col, BackTop, Icon,
 } from 'antd';
+import { Mobile, Tablet, Desktop } from '../containers/Responsive';
 
 const Footer = () => (
-  <Layout.Footer id="footer">
+  <Layout.Footer id="footer" style={{ zIndex: 0 }}>
     <Row type="flex" justify="space-between" align="middle">
-      <Col align="start">
-        <img height="20" alt="Centre universitaire de santé McGill" src="/images/mcgill.gif" />
-      </Col>
-      <Col align="center">
-        <img height="45" alt="Centre hospitalier urbain de Montréal" src="/images/chum.png" />
-      </Col>
-      <Col align="center">
-        <img height="40" alt="Hôpital général juif" src="/images/hgj.png" />
-      </Col>
-      <Col align="center">
-        <img height="42" alt="CHU du Québec" src="/images/chu.png" />
-      </Col>
-      <Col align="center">
-        <img height="40" alt="Hôpital Maisonneuve-Rosemont" src="/images/hmr.gif" />
-      </Col>
-      <Col align="center">
-        <img height="35" alt="Institut de cardiologie de Montréal" src="/images/icm.png" />
-      </Col>
-      <Col align="end">
-        <img height="50" alt="Centre hospitalier universitaire de Sherbrooke" src="/images/chus.gif" />
-      </Col>
+      <Desktop>
+        <Col span={3} align="start">
+          <img height="25" alt="Centre universitaire de santé McGill" src="/images/mcgill.gif" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="45" alt="Centre hospitalier urbain de Montréal" src="/images/chum.png" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="45" alt="Hôpital général juif" src="/images/hgj.png" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="42" alt="CHU du Québec" src="/images/chu.png" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="40" alt="Hôpital Maisonneuve-Rosemont" src="/images/hmr.gif" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="35" alt="Institut de cardiologie de Montréal" src="/images/icm.png" />
+        </Col>
+        <Col span={3} align="end">
+          <img height="50" alt="Centre hospitalier universitaire de Sherbrooke" src="/images/chus.gif" />
+        </Col>
+      </Desktop>
+      <Tablet>
+        <Col span={3} align="start">
+          <img height="25" alt="Centre universitaire de santé McGill" src="/images/mcgill.gif" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="45" alt="Centre hospitalier urbain de Montréal" src="/images/chum.png" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="45" alt="Hôpital général juif" src="/images/hgj.png" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="42" alt="CHU du Québec" src="/images/chu.png" />
+        </Col>
+        <Col span={3} align="start">
+          <img height="40" alt="Hôpital Maisonneuve-Rosemont" src="/images/hmr.gif" />
+        </Col>
+        <Col span={3} align="center">
+          <img height="30" alt="Institut de cardiologie de Montréal" src="/images/icm.png" />
+        </Col>
+        <Col span={3} align="end">
+          <img height="50" alt="Centre hospitalier universitaire de Sherbrooke" src="/images/chus.gif" />
+        </Col>
+      </Tablet>
+      <Mobile>
+        <Col span={4} align="start">
+          <img height="20" alt="Centre universitaire de santé McGill" src="/images/mcgill.gif" />
+        </Col>
+        <Col span={2} align="center">
+          <img height="40" alt="Centre hospitalier urbain de Montréal" src="/images/chum.png" />
+        </Col>
+        <Col span={4} align="center">
+          <img height="40" alt="Hôpital général juif" src="/images/hgj.png" />
+        </Col>
+        <Col span={2} align="center">
+          <img height="42" alt="CHU du Québec" src="/images/chu.png" />
+        </Col>
+        <Col span={3} align="end">
+          <img height="40" alt="Hôpital Maisonneuve-Rosemont" src="/images/hmr.gif" />
+        </Col>
+        <Col span={5} align="end">
+          <img height="30" alt="Institut de cardiologie de Montréal" src="/images/icm.png" />
+        </Col>
+        <Col span={4} align="end">
+          <img height="50" alt="Centre hospitalier universitaire de Sherbrooke" src="/images/chus.gif" />
+        </Col>
+      </Mobile>
     </Row>
     <BackTop>
       <Icon className="icon-large" type="up-circle" theme="filled" />
