c21210422870f4e70a5f2049cd432b9f43925776
claudia1296 <52966302+claudia1296@users.noreply.github.com>
2019-10-31 09:43:21 -0400

    Integration/98 home page

--- client/src/components/screens/Home/index.jsx
+++ client/src/components/screens/Home/index.jsx
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Card } from 'antd';
+import { Card, Typography, Row } from 'antd';
 
 import Header from '../../Header';
-import Navigation from '../../Navigation';
 import Content from '../../Content';
 import Footer from '../../Footer';
 import LoginForm from '../../forms/Login';
@@ -18,17 +17,37 @@ import './style.scss';
 
 const HomeScreen = ({ app, intl, actions }) => { // eslint-disable-line
   const { showLoadingAnimation } = app;
+  const { Paragraph } = Typography;
   window.CLIN.translate = intl.formatMessage;
+  const title = intl.formatMessage({ id: 'header.title' });
   return (
     <Content>
       <Header />
-      <Navigation />
       <Card className="centered">
         <LoginForm
           appIsLoading={showLoadingAnimation}
           handleAuthentication={actions.loginUser}
           handlePasswordRecovery={actions.recoverUser}
         />
+        <div className="content">
+          <img className="logo" alt={title} src="/images/logo_CQGC_couleur.svg" />
+          <Paragraph>
+            Le Centre québécois de génomique clinique offre une plateforme clinique de séquençage à haut débit pour le diagnostic moléculaire des patients québécois en partenariat avec les huit laboratoires du Réseau de diagnostic moléculaire du Québec.
+          </Paragraph>
+          <div className="partenaire">
+            <Row type="flex" justify="space-between" align="middle">
+              <img height="25" alt="Centre universitaire de santé McGill" src="/images/mcgill.gif" />
+              <img height="45" alt="Centre hospitalier urbain de Montréal" src="/images/chum.png" />
+              <img height="45" alt="Hôpital général juif" src="/images/hgj.png" />
+              <img height="42" alt="CHU du Québec" src="/images/chu.png" />
+            </Row>
+            <Row type="flex" justify="space-between" align="middle">
+              <img height="40" alt="Hôpital Maisonneuve-Rosemont" src="/images/hmr.gif" />
+              <img height="35" alt="Institut de cardiologie de Montréal" src="/images/icm.png" />
+              <img height="50" alt="Centre hospitalier universitaire de Sherbrooke" src="/images/chus.gif" />
+            </Row>
+          </div>
+        </div>
       </Card>
       <Footer />
     </Content>
