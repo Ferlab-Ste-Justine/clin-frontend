e728aa45ea74afdc77d0e3762f73bc988e6f33b6
claudia1296 <claudia.roy.cr@gmail.com>
2019-12-02 10:10:46 -0500

    Columns Action + style

--- client/src/components/Table/index.jsx
+++ client/src/components/Table/index.jsx
@@ -65,7 +65,7 @@ export const createCellRenderer = (type, getData, options = {}) => {
         const value = dataSet[row] ? dataSet[row][options.key] ? dataSet[row][options.key] : cloneDeep(dataSet[row]) : ''; // eslint-disable-line
 
         return (
-          <Cell>
+          <Cell className="cellValue">
             {valueRenderer(value)}
           </Cell>
         );
@@ -91,7 +91,6 @@ const DataTable = (props) => {
 
     reorderColumnsCallback(Utils.reorderArray(columns, oldIndex, newIndex, length));
   };
-
   return (
     <Table
       key={shortid.generate()}
@@ -104,6 +103,7 @@ const DataTable = (props) => {
       bodyContextMenuRenderer={renderContextMenuCallback}
       onColumnsReordered={handleColumnsReordered}
       onColumnWidthChanged={resizeColumnCallback}
+      defaultRowHeight={36}
     >
       { columns.map(definition => (
         <Column
