20e9fb45a19f8ff43f2a6f752abf0e5dbe7e0fb0
Nyan Of The Moon <nyanofthemoon@gmail.com>
2020-01-10 10:33:40 -0500

    removed fold/unfold queries

--- client/src/components/Query/Statement.jsx
+++ client/src/components/Query/Statement.jsx
@@ -668,13 +668,6 @@ class Statement extends React.Component {
                     onChange={this.onChange}
                     autocomplete="off"
                     value={(this.state.statementTitle ? this.state.statementTitle: statementTitle)}
-                    addonBefore={(
-                        <Button
-                            type="default"
-                        >
-                            <IconKit size={24} icon={ic_unfold_more} />
-                        </Button>
-                    )}
                     addonAfter={(
                         <div>
                           <Button
