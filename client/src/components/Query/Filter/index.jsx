ca91d0063060e6e35e5a1fa3433c9d47a313d457
claudia1296 <claudia.roy.cr@gmail.com>
2020-02-18 13:25:53 -0500

    reset button

--- client/src/components/Query/Filter/index.jsx
+++ client/src/components/Query/Filter/index.jsx
@@ -9,7 +9,7 @@ import {
 } from 'lodash';
 import IconKit from 'react-icons-kit';
 import {
-  ic_cancel, ic_info_outline, ic_search, ic_chevron_left, ic_arrow_drop_down,
+  ic_cancel, ic_info_outline, ic_search, ic_chevron_left, ic_arrow_drop_down, ic_replay,
 } from 'react-icons-kit/md';
 
 import style from '../styles/term.module.scss';
@@ -237,7 +237,7 @@ class Filter extends React.Component {
 
   render() {
     const {
-      onOperandChangeCallBack, config, data, draft, overlayOnly, editor, searchable, autoSelect,
+      onOperandChangeCallBack, config, data, draft, overlayOnly, editor, searchable, autoSelect, resettable,
     } = this.props;
     const {
       allOptions, size, page, visibleInput,
@@ -313,12 +313,19 @@ class Filter extends React.Component {
                   <IconKit size={16} className={styleFilter.iconInfo} icon={ic_info_outline} />
                 </Button>
               </Tooltip>
-              <Button className={styleFilter.iconSearch} onClick={this.handleInputView}>
-                <IconKit size={24} icon={ic_search} />
-              </Button>
+              {(searchable) && (
+                <Button className={styleFilter.iconSearch} onClick={this.handleInputView}>
+                  <IconKit size={24} icon={ic_search} />
+                </Button>
+              )}
+              {(resettable) && (
+                <Button className={styleFilter.iconSearch} onClick={this.handleInputView}>
+                  <IconKit size={24} icon={ic_replay} />
+                </Button>
+              )}
             </Row>
 
-            {searchable && (
+            {(searchable) && (
             <>
               <Row>
                 <Input
@@ -449,6 +456,7 @@ Filter.propTypes = {
   overlayOnly: PropTypes.bool,
   visible: PropTypes.bool,
   searchable: PropTypes.bool,
+  resettable: PropTypes.bool,
   sortData: PropTypes.array,
   autoSelect: PropTypes.bool,
   index: PropTypes.number,
@@ -473,6 +481,7 @@ Filter.defaultProps = {
   overlayOnly: false,
   visible: true,
   searchable: false,
+  resettable: false,
   sortData: [],
   index: 0,
 };
