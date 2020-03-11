c61d2263982dfe742dc45d3457d30e8987414cb6
Nyan Of The Moon <nyanofthemoon@gmail.com>
2020-03-11 16:03:13 -0400

    save state

--- client/src/containers/PrivateRoute.jsx
+++ client/src/containers/PrivateRoute.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 
-import { userShape } from '../reducers/user';
 import { isLoggedIn } from '../helpers/route';
 
 
@@ -18,14 +17,8 @@ const PrivateRoute = ({ Component, ...rest }) => (
   />
 );
 
-PrivateRoute.defaultProps = {
-  user: userShape,
-};
-
 PrivateRoute.propTypes = {
   Component: PropTypes.oneOfType([PropTypes.instanceOf(React.Component), PropTypes.func]).isRequired,
-  user: PropTypes.shape(userShape),
 };
-
 
 export default PrivateRoute;
--- client/src/sagas/router.js
+++ client/src/sagas/router.js
@@ -1,25 +1,12 @@
 import { push, LOCATION_CHANGE } from 'connected-react-router';
 import {
-  all, put, takeLatest, call,
+  all, put, takeLatest,
 } from 'redux-saga/effects';
 
 import * as actions from '../actions/type';
-import { fetchPatient, searchPatientsByQuery } from '../actions/patient';
 import {
   isPatientSearchRoute, isPatientPageRoutePattern, isPatientVariantPageRoutePattern, getPatientIdFromPatientPageRoute, getPatientIdFromPatientVariantPageRoute,
 } from '../helpers/route';
-
-
-function* navigate(action) {
-  try {
-    const { location } = action.payload;
-    yield put(push(location));
-    window.scrollTo(0, 0);
-    yield put({ type: actions.ROUTER_NAVIGATION_SUCCEEDED });
-  } catch (e) {
-    yield put({ type: actions.ROUTER_NAVIGATION_FAILED, message: e.message });
-  }
-}
 
 function* navigateToPatientScreen(action) {
   try {
@@ -38,6 +25,7 @@ function* navigateToPatientVariantScreen(action) {
   try {
     const { uid } = action.payload;
     const location = `/patient/${uid}/variant`;
+    yield put({ type: actions.PATIENT_FETCH_REQUESTED, payload: { uid } });
     yield put(push(location));
     window.scrollTo(0, 0);
     yield put({ type: actions.NAVIGATION_PATIENT_VARIANT_SCREEN_SUCCEEDED });
@@ -59,59 +47,24 @@ function* navigateToPatientSearchScreen() {
 }
 
 function* manualUserNavigation(action) {
-  const isPageReload = action.payload.isFirstRendering === true;
+  yield put({ type: actions.START_LOADING_ANIMATION });
+  yield put({ type: actions.START_SUBLOADING_ANIMATION });
   const location = action.payload.location.pathname;
-
   if (isPatientVariantPageRoutePattern(location)) {
-    console.log('+ isPatientVariantPageRoutePattern');
     const patientId = getPatientIdFromPatientPageRoute(location);
-
-
-    console.log(`+ isPatientVariantPageRoutePattern patientId ${JSON.stringify(patientId)}`);
-
-
-    yield fetchPatient(patientId);
     yield navigateToPatientVariantScreen({ payload: { uid: patientId } });
   } else if (isPatientPageRoutePattern(location)) {
-    console.log('+ isPatientPageRoutePattern');
     const patientId = getPatientIdFromPatientVariantPageRoute(location);
-
-
-    console.log(`+ isPatientPageRoutePattern patientId ${JSON.stringify(patientId)}`);
-
-
     yield navigateToPatientScreen({ payload: { uid: patientId } });
   } else if (isPatientSearchRoute(location)) {
-    console.log('+ isPatientSearchRoute');
-
-    yield call(searchPatientsByQuery);
-  } else {
-    // @TODO
+    yield navigateToPatientSearchScreen();
   }
-
-  yield console.log(`+ isPageReload ${JSON.stringify(isPageReload)}`);
-  yield console.log(`+ ${JSON.stringify(action.payload)}`);
-
-  /*
-  + TEST {
-  "type":"@@router/LOCATION_CHANGE",
-  "payload":{
-    "location":{
-      "pathname":"/patient/search",
-      "search":"",
-      "hash":"",
-      "key":"w1lwzc"},
-    "action":"POP",
-    "isFirstRendering":true}}
-   */
+  yield put({ type: actions.STOP_SUBLOADING_ANIMATION });
+  yield put({ type: actions.STOP_LOADING_ANIMATION });
 }
 
 function* watchManualUserNavigation() {
   yield takeLatest(LOCATION_CHANGE, manualUserNavigation);
-}
-
-function* watchNavigate() {
-  yield takeLatest(actions.ROUTER_NAVIGATION_REQUESTED, navigate);
 }
 
 function* watchNavigateToPatientScreen() {
@@ -128,7 +81,6 @@ function* watchNavigateToPatientVariantScreen() {
 
 export default function* watchedRouterSagas() {
   yield all([
-    watchNavigate(),
     watchNavigateToPatientScreen(),
     watchNavigateToPatientSearchScreen(),
     watchNavigateToPatientVariantScreen(),
