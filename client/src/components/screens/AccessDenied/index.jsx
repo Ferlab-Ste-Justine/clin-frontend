ad6184e6980444583bb5fed8df1c1169fd6c3d85
claudia1296 <52966302+claudia1296@users.noreply.github.com>
2020-04-21 10:00:32 -0400

    Styling/403 (#204)
    * facet dropdown
    
    * Page erreur 403

--- client/src/components/screens/AccessDenied/index.jsx
+++ client/src/components/screens/AccessDenied/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Typography } from 'antd';
 import intl from 'react-intl-universal';
+import IconKit from 'react-icons-kit';
+import {
+  ic_lock,
+} from 'react-icons-kit/md';
 
 import Header from '../../Header';
 import Content from '../../Content';
@@ -16,14 +20,22 @@ const goBack = () => {
 const AccessDeniedScreen = () => (
   <Content type="auto">
     <Header />
-    <Card className="entity">
-      <Row type="flex" gutter={32}>
-        <Col span={12} className="gutter-row">
-          {intl.get('screen.accessdenied.title')}
-          {intl.get('screen.accessdenied.description')}
-          {window.history.length > 2 ? <button type="button" onClick={goBack}>{intl.get('screen.accessdenied.button')}</button> : null }
-        </Col>
-      </Row>
+    <Card className="accessDeniedBackground">
+      <Card bordered={false} className="accessDeniedContent">
+        <Row>
+          <IconKit className="iconLock" size={72} icon={ic_lock} />
+        </Row>
+        <Row>
+          <Typography.Title className="title" level={4}> {intl.get('screen.accessdenied.title')}</Typography.Title>
+        </Row>
+        <Row>
+          <Typography.Text className="text">{intl.get('screen.accessdenied.description')}</Typography.Text>
+        </Row>
+        <Row>
+          {window.history.length > 2 ? <button type="button" className="buttonBack" onClick={goBack}>{intl.get('screen.accessdenied.button')}</button> : null }
+        </Row>
+      </Card>
+
     </Card>
     <Footer />
   </Content>
