e62996f2a0250ac3cfa4997da73fdcd7d7bc25ca
Francis Renaud <franciscaronrenaud@gmail.com>
2020-07-07 17:53:44 -0400

    Moved intl keys

--- client/src/locales/fr.js
+++ client/src/locales/fr.js
@@ -63,6 +63,11 @@ const fr = {
   'clinvar.value.Association': 'Association',
   'clinvar.value.Affects': 'Effets non pathologiques',
   'clinvar.value.Other': 'Autre',
+  'variant.report.header_value.Nucleotidic_variation_GRChv38': 'Variation Nucléotidique (GRChv38)',
+  'variant.report.header_value.Parental_origin': 'Statut (origine parentale)',
+  'variant.report.header_value.Allelic_frequency': 'Fréquence allélique',
+  'variant.report.header_value.Prediction_in_silico_sift': 'Prédiction in silico (Sift)',
+  'variant.report.header_value.ClinVar': 'ClinVar',
   'screen.accessdenied.title': 'Accès Refusé',
   'screen.accessdenied.description': "Nous sommes désolés, mais vous n'êtes pas authorisés à voir ce contenu.",
   'screen.accessdenied.button': 'Retourner à la page précédente',
@@ -461,11 +466,6 @@ const fr = {
   'components.table.action.createReport': 'Créer Rapport',
   'components.table.action.columns': 'Columns',
   'components.table.action.organize': 'Organizer',
-  'variant.report.header_value.Nucleotidic_variation_GRChv38': 'Variation Nucléotidique (GRChv38)',
-  'variant.report.header_value.Parental_origin': 'Statut (origine parentale)',
-  'variant.report.header_value.Allelic_frequency': 'Fréquence allélique',
-  'variant.report.header_value.Prediction_in_silico_sift': 'Prédiction in silico (Sift)',
-  'variant.report.header_value.ClinVar': 'ClinVar',
 };
 
 export default fr;
