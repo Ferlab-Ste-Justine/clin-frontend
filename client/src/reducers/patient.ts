e9f26596da8ba4bc5157c25dff274d6cf266c993
YanLabCHU <ymoussaoui@ferlab.bio>
2020-11-10 14:07:14 -0500

    Updates parsed patient data

--- client/src/reducers/patient.ts
+++ client/src/reducers/patient.ts
@@ -24,12 +24,16 @@ type ParsedPatientData = {
   status: string;
   lastName: string;
   firstName: string;
+  ramq: string;
+  mrn: string;
+  organization: string;
   gender: string;
   birthDate: string;
   ethnicity: string;
   bloodRelationship: string;
   familyId: string;
   familyType: string;
+  proband: string;
 };
 
 type PractitionerData = {
@@ -52,12 +56,13 @@ type Prescription = {
 // Reducer
 
 type State = {
-  patient: ParsedPatientData;
+  patient?: ParsedPatientData;
   prescriptions: Prescription[];
 };
 
 const ETHNICITY_EXT_URL = "http://fhir.cqgc.ferlab.bio/StructureDefinition/qc-ethnicity";
 const BLOOD_RELATIONSHIP_EXT_URL = "http://fhir.cqgc.ferlab.bio/StructureDefinition/blood-relationship";
+const PROBAND_EXT_URL = "http://fhir.cqgc.ferlab.bio/StructureDefinition/is-proband";
 
 type BundleType = "Patient" | "ServiceRequest" | "ClinicalImpression" | "FamilyGroup";
 
@@ -66,11 +71,11 @@ class PatientPageParser {
     switch (type) {
       case "Patient":
         return data.entry[0].resource;
-      case "ServiceRequest":
-        return data.entry[1].resource;
-      case "ClinicalImpression":
-        return data.entry[2].resource;
       case "FamilyGroup":
+        return data.entry[1].resource;
+      case "ServiceRequest":
+        return data.entry[2].resource;
+      case "ClinicalImpression":
         return data.entry[3].resource;
       default:
         throw new Error(`Invalid bundle type ${type}`);
@@ -93,32 +98,42 @@ class PatientPageParser {
 
   public static parse(data: any): Partial<State> {
     const patientBundle = this.extractBundle(data, "Patient");
-    const groupBundle = this.extractBundle(data, "Patient");
+    const groupBundle = this.extractBundle(data, "FamilyGroup");
 
-    const patient = this.extractResource<Patient>(data, "Patient");
+    const patient = this.extractResource<Patient>(patientBundle, "Patient");
     const practitioner = this.extractResource<Practitioner>(patientBundle, "Practitioner");
     const organization = this.extractResource<Organization>(patientBundle, "Organization");
     const group = this.extractResource<FamilyGroup>(groupBundle, "Group");
 
     const ethnicityExt = this.getExtension(patient, ETHNICITY_EXT_URL);
     const bloodRelationshipExt = this.getExtension(patient, BLOOD_RELATIONSHIP_EXT_URL);
+    const probandExt = this.getExtension(patient, PROBAND_EXT_URL);
+
+    const mrn = patient.identifier[0].value;
+    const ramq = has(patient, "identifier[1].value") ? patient.identifier[1].value : "N/A";
 
     const patientData: ParsedPatientData = {
       id: patient.id,
       status: patient.active ? "active" : "inactive",
       lastName: patient.name[0].family,
       firstName: patient.name[0].given[0],
+      ramq,
+      mrn,
+      organization: organization.name || organization.id,
       gender: patient.gender,
       birthDate: patient.birthDate,
       ethnicity:
         ethnicityExt != null && has(ethnicityExt, "valueCoding.display") ? ethnicityExt.valueCoding.display : "N/A",
       bloodRelationship:
-        bloodRelationshipExt != null && has(bloodRelationshipExt, "valueCoding.displau")
+        bloodRelationshipExt != null && has(bloodRelationshipExt, "valueCoding.display")
           ? bloodRelationshipExt.valueCoding.display
           : "N/A",
       familyId: group.id,
       familyType: group.type,
+      proband: probandExt != null && probandExt.valueBoolean ? "Proband" : "Parent",
     };
+
+    console.log(patientData);
 
     return {
       patient: patientData,
@@ -132,10 +147,12 @@ type Action = {
   payload: any;
 };
 
-const reducer = (state: State, action: Action) =>
+const reducer = (state: State = { prescriptions: [] }, action: Action) =>
   produce<State>(state, (draft) => {
     switch (action.type) {
       case "PATIENT_FETCH_SUCCEEDED":
+        console.log(action);
+        PatientPageParser.parse(action.payload);
         break;
     }
   });
