fee624b92e72f0cab22c8d9a34667a769e405165
YanLabCHU <ymoussaoui@ferlab.bio>
2020-11-12 13:12:39 -0500

    feat: #491 Adds HPOS Provider

---
+++ client/src/helpers/providers/hpos/index.ts
@@ -0,0 +1,40 @@
+import { get } from "lodash";
+import { Observation } from "../../fhir/types";
+import { ClinicalObservation } from "../types";
+//@ts-ignore
+import { DataExtractor } from "../extractor.ts";
+//@ts-ignore
+import { Provider, Record } from "../providers.ts";
+
+const AGE_AT_ONSET_EXTENSION = "http://fhir.cqgc.ferlab.bio/StructureDefinition/age-at-onset";
+
+const HPO_CODE = "PHENO";
+export class HPOProvider extends Provider<Observation, ClinicalObservation> {
+  constructor(name: string) {
+    super(name);
+  }
+
+  public doProvide(dataExtractor: DataExtractor): Record<Observation, ClinicalObservation>[] {
+    const clinicalImpressionBundle = dataExtractor.extractBundle("ClinicalImpression");
+
+    const hpos = dataExtractor
+      .extractResources<Observation>(clinicalImpressionBundle, "Observation")
+      .filter((observation) => get(observation, ["code", "coding", "0", "code"], "") === HPO_CODE);
+
+    return hpos.map<Record<Observation, ClinicalObservation>>((hpo) => {
+      const ageAtOnset = dataExtractor.getExtension(hpo, AGE_AT_ONSET_EXTENSION);
+
+      const observation: ClinicalObservation = {
+        observed: get(hpo, "interpretation[0].coding[0].code", "IND"),
+        term: get(hpo, "valueCodeableConcept.coding[0].code", "N/A"),
+        ageAtOnset: get(ageAtOnset, "valueCoding.display", "N/A"),
+        note: get(hpo, "note[0].text", ""),
+      };
+
+      return {
+        original: hpo,
+        parsed: observation,
+      };
+    });
+  }
+}
