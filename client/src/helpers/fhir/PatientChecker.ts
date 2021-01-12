df6383de5136ffc5b72b1281257551dc4de7e56e
YanLabCHU <ymoussaoui@ferlab.bio>
2021-01-12 14:15:36 -0500

    feat: #613 gets patient info by identifier

---
+++ client/src/helpers/fhir/PatientChecker.ts
@@ -0,0 +1,14 @@
+import Http from '../http-client';
+
+const RAMQ_PATTERN = RegExp(/^[a-zA-Z-]{4}\d{8,9}$/);
+
+const isValidRamq = (ramq: string) => RAMQ_PATTERN.test(ramq);
+
+export const getPatientByRamq = (ramq: string) => {
+  if (!isValidRamq(ramq)) {
+    throw new Error('Invalid RAMQ');
+  }
+
+  return Http.secureClinAxios.get(`${window.CLIN.fhirBaseUrl}/Patient?identifier=${ramq}`).then((payload) => ({ payload }))
+    .catch((error) => ({ error }));
+};
