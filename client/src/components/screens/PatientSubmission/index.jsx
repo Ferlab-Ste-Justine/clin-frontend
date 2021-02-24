a5baa55b37638bf482c7821d3a732e21c7d936b6
YanLabCHU <ymoussaoui@ferlab.bio>
2021-02-24 08:57:42 -0500

    feat: #727 Save mrn in prescription form

--- client/src/components/screens/PatientSubmission/index.jsx
+++ client/src/components/screens/PatientSubmission/index.jsx
@@ -324,9 +324,12 @@ function PatientSubmissionScreen(props) {
         return;
       }
 
+      const [mrn, hospital] = content.mrn.split('|');
+
       allAnalysis.forEach((analysis) => {
         batch.serviceRequests.push(new ServiceRequestBuilder()
           .withId(get(localStore, 'serviceRequest.id'))
+          .withMrn(mrn, hospital)
           .withRequester(state.selectedPractitioner)
           .withSubject(currentPatient.id)
           .withCoding(getTestCoding(analysis))
--- client/src/helpers/fhir/types.ts
+++ client/src/helpers/fhir/types.ts
@@ -161,6 +161,7 @@ export interface ServiceRequest {
   status: string;
   intent: string;
   authoredOn: string;
+  identifier: Identifier[];
   category: Category[];
   priority: string;
   code?: CodeableConcept;
