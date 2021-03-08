773891bd439fb023465f72f33af3704b83971b68
YanLabCHU <ymoussaoui@ferlab.bio>
2021-03-08 11:38:25 -0500

    fix: #747 edit draft prescription from 'complete' link

--- client/src/components/screens/Patient/components/PrescriptionsTab/components/Prescriptions.tsx
+++ client/src/components/screens/Patient/components/PrescriptionsTab/components/Prescriptions.tsx
@@ -142,7 +142,7 @@ const Prescriptions : React.FC<Props> = ({ prescriptions, clinicalImpressions })
                         <Button
                           type="link"
                           size="small"
-                          onClick={() => dispatch(navigateToSubmissionWithPatient())}
+                          onClick={() => dispatch(editPrescription(prescription.id))}
                         >
                           { intl.get('screen.patient.details.prescription.alert.action') }
                         </Button>
