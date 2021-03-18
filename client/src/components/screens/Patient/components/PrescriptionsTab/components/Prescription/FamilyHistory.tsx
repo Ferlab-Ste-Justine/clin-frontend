8962ca2217b15953b2ad11b690add63978f75d52
YanLabCHU <ymoussaoui@ferlab.bio>
2021-03-18 10:38:54 -0400

    fix: #754 Display ethnicity note

--- client/src/components/screens/Patient/components/PrescriptionsTab/components/Prescription/FamilyHistory.tsx
+++ client/src/components/screens/Patient/components/PrescriptionsTab/components/Prescription/FamilyHistory.tsx
@@ -14,6 +14,13 @@ const Wrapper: React.FC = ({ children }) => (
   </Card>
 );
 
+const EthnicityNote: React.FC<{note?: string}> = ({ note }) => {
+  if (note == null) {
+    return <></>;
+  }
+  return <span className="prescriptions-tab__prescriptions-section__family-history__ethnicity-note"> | { note }</span>;
+};
+
 interface Observations{
   eth?: Observation
   cons?: Observation
@@ -23,12 +30,25 @@ interface Props {
   observations: Observations
 }
 
-const getEthnicity = (ethnicityObs?: Observation) => {
+interface EthnicityData{
+  code: string;
+  note?: string;
+}
+
+const getEthnicity = (ethnicityObs?: Observation): EthnicityData => {
   const ethnicity = get(ethnicityObs, 'valueCodeableConcept.coding[0].code', undefined);
+  const note = get(ethnicityObs, 'note[0].text');
+
   if (ethnicity !== undefined) {
-    return intl.get(`form.patientSubmission.form.ethnicity.${ethnicity}`);
+    return {
+      code: intl.get(`form.patientSubmission.form.ethnicity.${ethnicity}`),
+      note,
+    };
   }
-  return '--';
+  return {
+    code: '--',
+    note,
+  };
 };
 
 const getConsanguinity = (consanguinityObs?: Observation) => {
@@ -42,7 +62,7 @@ const getConsanguinity = (consanguinityObs?: Observation) => {
 const FamilyHistory: React.FC<Props> = ({ familyHistories, observations }) => {
   const ethnicity = getEthnicity(observations.eth);
   const consanguinity = getConsanguinity(observations.cons);
-  if (consanguinity === '--' && ethnicity === '--' && familyHistories.length === 0) {
+  if (consanguinity === '--' && ethnicity.code === '--' && familyHistories.length === 0) {
     return (
       <Wrapper>
         <Typography.Text className="prescriptions-tab__prescriptions-section--empty">
@@ -55,7 +75,8 @@ const FamilyHistory: React.FC<Props> = ({ familyHistories, observations }) => {
   return (
     <Wrapper>
       <DetailsRow label={intl.get('screen.patient.details.prescriptions.family.ethnicity')}>
-        { ethnicity }
+        { ethnicity.code }
+        <EthnicityNote note={ethnicity.note} />
       </DetailsRow>
       <DetailsRow label={intl.get('screen.patient.details.prescriptions.family.blood')}>
         { consanguinity }
