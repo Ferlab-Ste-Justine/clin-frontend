392a3ab66e6e392ac811bbe6cee263e46743ea56
Francis Provost <me@francisprovost.com>
2021-04-21 14:58:35 -0400

    fix: fix e2e tests start command

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -6,6 +6,7 @@ module.exports = (on: Cypress.PluginEvents, config: Cypress.ConfigOptions) => {
   }
   config.env.username = process.env.CYPRESS_USERNAME;
   config.env.password = process.env.CYPRESS_PASSWORD;
+  config.env.patientFhirUrl = process.env.REACT_APP_PATIENT_SERVICE_URL;
 
   return config;
 };
