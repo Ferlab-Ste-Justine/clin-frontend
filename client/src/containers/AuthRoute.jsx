1c20e6e709ca98e0d571aa07abba8bba640570f6
YanLabCHU <ymoussaoui@ferlab.bio>
2021-05-06 11:38:52 -0400

    feat: #849 Update Authroute to check for permissions

--- client/src/containers/AuthRoute.jsx
+++ client/src/containers/AuthRoute.jsx
@@ -2,38 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 import { useKeycloak } from '@react-keycloak/web';
+import { useSelector } from 'react-redux';
 
-const AuthRoute = ({ Component, roles, ...rest }) => {
+const AuthRoute = ({
+  Component, resource, ...rest
+}) => {
   const { keycloak } = useKeycloak();
+  const user = useSelector((state) => state.user);
 
-  const isAuthorized = (requiredRoles) => {
-    // We check if Keycloak is initialized in the AppRouter.jsx, thus no need to test it here.
-    if (keycloak && !keycloak.authenticated) {
-      keycloak.login();
-    }
-
-    if (!requiredRoles || requiredRoles.length === 0) {
-      return true;
-    }
-
-    if (keycloak) {
-      if (requiredRoles && requiredRoles.length > 0) {
-        return requiredRoles.some((r) => {
-          const realm = keycloak.hasRealmRole(r);
-          const resource = keycloak.hasResourceRole(r);
-          return realm || resource;
-        });
-      }
-      return keycloak.authenticated;
-    }
-    return false;
-  };
+  const isAuthorized = () => keycloak && user.permissions.includes(resource);
 
   return (
     <Route
       {...rest}
       render={(props) => (
-        isAuthorized(roles)
+        isAuthorized()
           ? <Component {...props} />
           : (
             <Redirect
@@ -50,7 +33,6 @@ const AuthRoute = ({ Component, roles, ...rest }) => {
 
 AuthRoute.propTypes = {
   Component: PropTypes.oneOfType([PropTypes.instanceOf(React.Component), PropTypes.func]).isRequired,
-  roles: PropTypes.arrayOf(PropTypes.string).isRequired,
   match: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
