64a04ad9d802cea020b00a80b34334c7ce8b3d4f
YanLabCHU <ymoussaoui@ferlab.bio>
2021-05-20 14:46:03 -0400

    feat: #849 Set RPT header on fhir request

--- client/src/helpers/http-client.js
+++ client/src/helpers/http-client.js
@@ -1,16 +1,65 @@
 import Axios from 'axios';
 import keycloak from '../keycloak';
+import { RptManager } from './keycloak-api/manager';
 
 const secureClinAxios = Axios.create({
   timeout: 15000,
   // withCredentials: true
 });
 secureClinAxios.defaults.crossdomain = true;
-secureClinAxios.interceptors.request.use((config) => {
+secureClinAxios.interceptors.request.use(async (config) => {
   config.headers.Authorization = `Bearer ${keycloak.token}`;
   // config.headers['Accept-Language'] = 'fr-CA'
   return config;
 });
+
+const { post } = secureClinAxios;
+const { get } = secureClinAxios;
+const { put } = secureClinAxios;
+
+secureClinAxios.post = async (url, data, config = {}) => {
+  if (!url.startsWith(window.CLIN.fhirBaseUrl)) {
+    return post(url, data, config);
+  }
+
+  const rpt = await RptManager.readRpt();
+
+  return post(url, data, {
+    ...config,
+    headers: {
+      RPT: rpt.accessToken,
+    },
+  });
+};
+
+secureClinAxios.get = async (url, config = {}) => {
+  if (!url.startsWith(window.CLIN.fhirBaseUrl)) {
+    return get(url, config);
+  }
+  const rpt = await RptManager.readRpt();
+
+  return get(url, {
+    ...config,
+    headers: {
+      RPT: rpt.accessToken,
+    },
+  });
+};
+
+secureClinAxios.put = async (url, data, config) => {
+  if (!url.startsWith(window.CLIN.fhirBaseUrl)) {
+    return put(url, data, config);
+  }
+  const rpt = await RptManager.readRpt();
+
+  return put(url, data, {
+    ...config,
+    headers: {
+      RPT: rpt.accessToken,
+    },
+  });
+};
+
 export default {
   secureClinAxios,
 };
