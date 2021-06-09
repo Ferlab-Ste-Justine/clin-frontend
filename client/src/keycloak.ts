37e4046ba67fbaa979e965b624567fe66c99a63e
Eric Vallee <eric_vallee@webificservices.com>
2021-06-09 14:37:37 -0400

    feat: CLIN-52 Started making urls dynamically configurable

--- client/src/keycloak.ts
+++ client/src/keycloak.ts
@@ -1,6 +1,10 @@
 import Keycloak from 'keycloak-js';
 
-const keyCloakConfig = JSON.parse(process.env.REACT_APP_KEYCLOAK_CONFIG!);
+declare global {
+    interface Window { env: any; }
+}
+
+const keyCloakConfig = JSON.parse(window.env.REACT_APP_KEYCLOAK_CONFIG || process.env.REACT_APP_KEYCLOAK_CONFIG!);
 const keycloak = Keycloak(keyCloakConfig);
 
 export default keycloak;
