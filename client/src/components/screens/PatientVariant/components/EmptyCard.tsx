82805f3174d4462d874394c4b2559dab7b300ef0
claudia1296 <claudia.roy.cr@gmail.com>
2021-06-23 13:45:50 -0400

    ajout page aucun variant

---
+++ client/src/components/screens/PatientVariant/components/EmptyCard.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Card, Typography, Button,
+} from 'antd';
+import React from 'react';
+import IconKit from 'react-icons-kit';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  ic_widgets,
+} from 'react-icons-kit/md';
+import intl from 'react-intl-universal';
+import { State } from '../../../../reducers';
+import { navigateToPatientScreen } from '../../../../actions/router';
+
+const EmptyCard: React.FC = () => {
+  const dispatch = useDispatch();
+  const patient = useSelector((state: State) => state.patient.patient.parsed);
+  const handleGoToPrescriptionTab = () => {
+    console.log('changement Onglet');
+  };
+
+  return (
+    <Card className="family-tab__details" bordered={false}>
+      <div className="family-tab__details--empty">
+        <IconKit size={42} icon={ic_widgets} />
+        <Typography.Text className="family-tab__details--empty__texts__title">
+          { intl.get('screen.patient.details.variant.empty.title') }
+        </Typography.Text>
+        <Typography.Text className="family-tab__details--empty__texts__description">
+          { intl.get('screen.patient.details.variant.empty.description') }
+          <Button type="link" onClick={handleGoToPrescriptionTab}>
+            { intl.get('screen.patient.details.variant.empty.link') }
+          </Button>
+        </Typography.Text>
+      </div>
+    </Card>
+  );
+};
+
+export default EmptyCard;
