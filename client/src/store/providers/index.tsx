170a8189d66d996bcb0365b64e56a4a14bee93ea
Francis Lavoie <flavoie@ferlab.bio>
2021-08-25 14:40:50 -0400

    feat: added apollo/graphql

---
+++ client/src/store/providers/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ApolloProvider from './apollo';
+
+export interface IProvider {
+  children: React.ReactNode;
+  userToken: string;
+}
+
+const Provider = ({ children, userToken }: IProvider): React.ReactElement => (
+  <ApolloProvider userToken={userToken}>{ children }</ApolloProvider>
+);
+
+export default Provider;
