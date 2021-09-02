c81ea1b012deb7a8526e6e618b725aede3918d98
Francis Lavoie <flavoie@ferlab.bio>
2021-09-02 09:05:03 -0400

    feat: #251support multi backend for apollo provider

--- client/src/store/providers/apollo.tsx
+++ client/src/store/providers/apollo.tsx
@@ -6,13 +6,18 @@ import {
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client';
+
 import { setContext } from '@apollo/client/link/context';
-import { IProvider } from 'store/providers';
+import { GraphqlBackend, GraphqlProvider } from 'store/providers';
 
 const ARRANGER_API = process.env.REACT_APP_ARRANGER_API;
 const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
 
-const httpLink = createHttpLink({
+const fhirLink = createHttpLink({
+  uri: `${process.env.REACT_APP_FHIR_SERVICE_URL}/$graphql`,
+});
+
+const arrangerLink = createHttpLink({
   uri: `${ARRANGER_API}${PROJECT_ID}/graphql`,
 });
 
@@ -23,12 +28,16 @@ const getAuthLink = (userToken: string) => setContext((_, { headers }) => ({
   },
 }));
 
-export default ({ children, userToken }: IProvider): ReactElement => {
+const backendUrl = (backend: GraphqlBackend) => (
+  backend === GraphqlBackend.FHIR ? fhirLink : arrangerLink
+);
+
+export default ({ children, backend = GraphqlBackend.FHIR, userToken }: GraphqlProvider): ReactElement => {
   const header = getAuthLink(userToken);
 
   const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache: new InMemoryCache({ addTypename: false }),
-    link: header.concat(httpLink),
+    link: header.concat(backendUrl(backend)),
   });
   return <ApolloProvider client={client}>{ children }</ApolloProvider>;
 };
--- client/src/store/providers/index.tsx
+++ client/src/store/providers/index.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 import ApolloProvider from './apollo';
 
+export enum GraphqlBackend {
+  FHIR,
+  ARRANGER
+}
+
+type ClinGraphqlProvider = {
+  backend?: GraphqlBackend;
+}
+
 export interface IProvider {
   children: React.ReactNode;
   userToken: string;
 }
+
+export type GraphqlProvider = IProvider & ClinGraphqlProvider;
 
 const Provider = ({ children, userToken }: IProvider): React.ReactElement => (
   <ApolloProvider userToken={userToken}>{ children }</ApolloProvider>
