9b196752062b60fbde99c416bc371cdb9a1ae321
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-09-17 14:28:23 -0400

    Add pro table for variant search page

--- client/src/components/screens/PatientVariant/VariantTableContainer.tsx
+++ client/src/components/screens/PatientVariant/VariantTableContainer.tsx
@@ -1,25 +1,79 @@
-import React, { useState } from 'react';
-import { Table } from 'antd';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-curly-spacing */
 
+import React, { useState } from 'react';
+import type { ProColumns } from '@ant-design/pro-table';
+import ProTable from '@ant-design/pro-table';
+
+import '../../../../../node_modules/@ant-design/pro-table/dist/table.css';
 import style from './VariantTableContainer.module.scss';
 
 const DEFAULT_PAGE_NUM = 1;
+const DEFAULT_PAGE_SIZE = 10;
 
 type OwnProps = {
   setCurrentPageCb: (currentPage: number) => void;
 };
 
+const columns: ProColumns[] = [
+  {
+    title: 'test1',
+    dataIndex: 'test1',
+  },
+  {
+    title: 'test2',
+    dataIndex: 'test2',
+  },
+  {
+    title: 'test3',
+    dataIndex: 'test3',
+  },
+  {
+    title: 'test4',
+    dataIndex: 'test4',
+  },
+];
+
+const defaultData = [
+  {
+    test1: 'Allo',
+    test2: 'Allo',
+    test3: 'Allo',
+    test4: 'Allo',
+  },
+];
+
 const VariantTableContainer = (props: OwnProps) => {
   const { setCurrentPageCb } = props;
   const [currentPageNum, setCurrentPageNum] = useState(DEFAULT_PAGE_NUM);
-
   const total = 0;
 
   return (
-    <Table
+    <ProTable
+      columns={columns}
+      search={false}
+      toolbar={{
+        title: (
+          <div className={style.tabletotalTitle}>
+            Résultats <strong>1 - {DEFAULT_PAGE_SIZE}</strong> sur <strong>200</strong>
+          </div>
+        ),
+      }}
+      defaultData={defaultData}
+      className={style.variantSearchTable}
+      options={{
+        density: false,
+        reload: false,
+      }}
+      cardBordered={true}
       pagination={{
         current: currentPageNum,
-        total,
+        showTotal: () => undefined,
+        showTitle: false,
+        showSizeChanger: false,
+        showQuickJumper: false,
         onChange: (page) => {
           if (currentPageNum !== page) {
             setCurrentPageNum(page);
@@ -28,10 +82,6 @@ const VariantTableContainer = (props: OwnProps) => {
         },
         size: 'small',
       }}
-      className={style.variantSearchTable}
-      loading={false}
-      dataSource={[]}
-      columns={[]}
     />
   );
 };
