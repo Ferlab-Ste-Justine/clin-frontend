a4dccf28a31d2b323dea041057b525d5a228f2b0
evans-g-crsj <eg.crsj@gmail.com>
2021-09-28 10:20:25 -0400

    refactor: create test directories

--- client/src/components/screens/Patient/components/FamilyTab/tests/family.spec.tsx
+++ client/src/components/screens/Patient/components/FamilyTab/tests/family.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AppTest from 'AppTest';
 
-import FamilyTab from './index';
+import FamilyTab from '../index';
 
 const patientSubState = {
   patient: {
--- client/src/components/screens/Patient/components/PatientDetails/tests/profileCard.spec.tsx
+++ client/src/components/screens/Patient/components/PatientDetails/tests/profileCard.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AppTest from 'AppTest';
 
-import ProfileCard from './ProfileCard';
+import ProfileCard from '../ProfileCard';
 
 const patientSubState = {
   patient: {
