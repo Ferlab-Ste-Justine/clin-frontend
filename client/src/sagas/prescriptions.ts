7f54d53aa262a7d30253e96d2364b89e793263af
claudia1296 <claudia.roy.cr@gmail.com>
2021-11-02 12:28:53 -0400

    clin-129: afficher bon onglet prescription

--- client/src/sagas/prescriptions.ts
+++ client/src/sagas/prescriptions.ts
@@ -1,8 +1,8 @@
+import * as actions from 'actions/type';
+import { createRequest, CreateRequestBatch, updateRequest } from 'helpers/fhir/api/CreateRequest';
 import {
-  all, put, takeLatest, select,
+  all, put, select, takeLatest, 
 } from 'redux-saga/effects';
-import * as actions from '../actions/type';
-import { createRequest, CreateRequestBatch, updateRequest } from '../helpers/fhir/api/CreateRequest';
 
 function* handleCreateRequest(action: any) {
   try {
@@ -10,12 +10,10 @@ function* handleCreateRequest(action: any) {
     const batch = action.payload.batch as CreateRequestBatch;
     const handler = batch.update ? updateRequest : createRequest;
     const response = yield handler(batch);
-
-    yield put({ type: actions.CREATE_PATIENT_REQUEST_SUCCEEDED, payload: { ...response } });
-
+    yield put({ payload: { ...response }, type: actions.CREATE_PATIENT_REQUEST_SUCCEEDED });
     yield put({
+      payload: { openedPrescriptionId: response.serviceRequests[0].id, reload: true, uid: patient.id },
       type: actions.NAVIGATION_PATIENT_SCREEN_REQUESTED,
-      payload: { uid: patient.id, reload: true, openedPrescriptionId: action.payload.openedPrescriptionId },
     });
   } catch (error) {
     yield put({ type: actions.CREATE_PATIENT_REQUEST_FAILED });
