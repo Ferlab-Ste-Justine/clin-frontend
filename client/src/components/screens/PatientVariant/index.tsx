e15fc535000c2fcc6c7459a2f2e40f8d3fd35c2f
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-11-17 12:07:39 -0500

    CLIN-453: Add iframe with env var url

--- client/src/components/screens/PatientVariant/index.tsx
+++ client/src/components/screens/PatientVariant/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { useLocation } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import VariantPageV1 from './VariantSearchV1';
+import keycloak from 'keycloak';
 
-import styles from "./index.module.scss";
+import styles from './index.module.scss';
+import EnvironmentVariables from 'helpers/EnvironmentVariables';
 
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
-}
+};
 
 const index = (props: any) => {
   const query = useQuery();
+  const { uid } = useParams<{ uid: string }>();
 
-  if (true) {
-    return <iframe className={styles.variantIframe} src="http://localhost:2005/variant/QA-PA-00112" />;
-  } else {
+  if (query.get('v') == '1') {
     return <VariantPageV1 {...props} />;
+  } else {
+    return (
+      <iframe
+        className={styles.variantIframe}
+        src={`${EnvironmentVariables.configFor({ key: 'CLIN_UI' })}/variant/${uid}?token=${
+          keycloak.token
+        }`}
+      />
+    );
   }
 };
 
