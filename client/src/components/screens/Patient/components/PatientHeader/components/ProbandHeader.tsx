5cb2bff8cd45aa8112ce5ed3f0401544363c3c5f
claudia1296 <claudia.roy.cr@gmail.com>
2021-11-22 11:21:28 -0500

    clin-417: Changement du header page patient

---
+++ client/src/components/screens/Patient/components/PatientHeader/components/ProbandHeader.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import intl from 'react-intl-universal';
+import { Col, Tag, Typography } from 'antd';
+import { ParsedPatientData } from 'helpers/providers/types';
+
+import FemaleIcon from 'components/Assets/Icons/FemaleIcon'
+import MaleIcon from 'components/Assets/Icons/MaleIcon';
+import UnknowGenderIcon from 'components/Assets/Icons/UnknowGenderIcon'
+
+import '../styles.scss';
+  
+enum Gender {
+MALE = 'male',
+FEMALE = 'female',
+UNKNOWN = 'unknown',
+}  
+
+const getGenderIcon = (gender: string) => {
+  if (gender === Gender.FEMALE) {
+    return (
+      <FemaleIcon className="customIcon" height="22" viewBox="0 0 22 22" width="22" />
+    );
+  }else if (gender === Gender.UNKNOWN){
+    return (
+      <UnknowGenderIcon className="customIcon" height="23" viewBox="0 0 22 23" width="22" />
+    );
+  }else if (gender === Gender.MALE){
+    return ( 
+      <MaleIcon className="customIcon" height="22" viewBox="0 0 22 22" width="22"/>
+    )
+  }
+};
+
+interface Props {
+    patient: ParsedPatientData
+  }
+
+const ProbandHeader= ({ patient }:Props): React.ReactElement => (
+  <>
+    <Col>
+      <Typography.Title className="patient-page__header__name" level={3}>
+        { `${patient.lastName.toUpperCase()} ${patient.firstName}` }
+      </Typography.Title>
+    </Col>
+    <Col>
+      <Typography.Title className='patient-page__header__id' level={4}>
+            (ID: {patient.id})
+      </Typography.Title>
+    </Col>
+    <Col>
+      {getGenderIcon(patient.gender)}
+    </Col>
+    <Col>
+      <Tag
+        className="patient-page__header__tags"
+        color={patient.proband === 'Parent' ? 'geekblue' : 'red'}
+      >
+        {patient.proband}
+      </Tag>
+    </Col>
+    <Col>
+      <Tag
+        className="patient-page__header__tags"
+      >
+        <span>RAMQ :</span> {patient.ramq}
+      </Tag>
+    </Col>
+    {
+      patient.birthDate && (
+        <Col>
+          <Tag className="patient-page__header__tags">
+            <span>{ intl.get('screen.patient.details.header.dob') } :</span>{ patient.birthDate }
+          </Tag>
+        </Col>
+      )
+    }
+  </>
+);
+
+export default ProbandHeader;
