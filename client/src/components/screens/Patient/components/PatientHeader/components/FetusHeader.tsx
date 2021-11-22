98c7c18c4c446dd99d31700551a60868b651e572
claudia1296 <claudia.roy.cr@gmail.com>
2021-11-22 11:21:28 -0500

    clin-417: Changement du header page patient

---
+++ client/src/components/screens/Patient/components/PatientHeader/components/FetusHeader.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import intl from 'react-intl-universal';
+import { useDispatch } from 'react-redux';
+import { navigateToPatientScreen } from 'actions/router';
+import { Button, Col, Tag, Typography } from 'antd';
+import { ParsedPatientData } from 'helpers/providers/types';
+
+import UnknowGenderIcon from 'components/Assets/Icons/UnknowGenderIcon'
+
+import '../styles.scss';
+  
+
+interface Props {
+    patient: ParsedPatientData
+  }
+
+const FetusHeader= ({ patient }:Props): React.ReactElement => {
+  const dispatch = useDispatch();
+  return(  
+    <>
+      <Col>
+        <Typography.Title className="patient-page__header__name" level={3}>
+        ID : {patient.id}
+        </Typography.Title>
+      </Col>
+      <Col>
+        <UnknowGenderIcon className="customIcon" height="23" viewBox="0 0 22 23" width="22" />
+      </Col>
+      <Col>
+        <Tag
+          className="patient-page__header__tags"
+          color="purple"
+        >
+          { intl.get('screen.patient.details.header.fetus') }
+        </Tag>
+      </Col>
+      <Col>
+        <Tag
+          className="patient-page__header__tags"
+        >
+          <span>{ intl.get('screen.patient.details.header.mother') } :</span> 
+          <Button 
+            className="link--underline link--small" 
+            onClick={() => dispatch(navigateToPatientScreen(patient.familyRelation))}
+            size='small' 
+            type='link'
+          >
+            { `${patient.lastName.toUpperCase()} ${patient.firstName} (ID : ${patient.familyRelation})` }
+          </Button>
+        </Tag>
+      </Col>
+    </>
+  )
+};
+
+export default FetusHeader;
--- client/src/components/screens/Patient/components/PatientHeader/index.tsx
+++ client/src/components/screens/Patient/components/PatientHeader/index.tsx
@@ -1,115 +1,28 @@
 import React from 'react';
-import intl from 'react-intl-universal';
-import { useDispatch } from 'react-redux';
-import { navigateToPatientScreen } from 'actions/router';
-import {
-  Button,
-  Col,   Row, Tag, Typography, } from 'antd';
+import { Row, } from 'antd';
 import { ParsedPatientData } from 'helpers/providers/types';
 
-import FemaleIcon from 'components/Assets/Icons/FemaleIcon'
-import MaleIcon from 'components/Assets/Icons/MaleIcon';
-import UnknowGenderIcon from 'components/Assets/Icons/UnknowGenderIcon'
+import FetusHeader from './components/FetusHeader';
+import ProbandHeader from './components/ProbandHeader';
 
 import './styles.scss';
-
-enum Gender {
-  MALE = 'male',
-  FEMALE = 'female',
-  UNKNOWN = 'unknown',
-
-}
-
-const getGenderIcon = (gender: string) => {
-  if (gender === Gender.FEMALE) {
-    return (
-      <FemaleIcon className="customIcon" height="22" viewBox="0 0 22 22" width="22" />
-    );
-  }else if (gender === Gender.UNKNOWN){
-    return (
-      <UnknowGenderIcon className="customIcon" height="23" viewBox="0 0 22 23" width="22" />
-    );
-  }else if (gender === Gender.MALE){
-    <MaleIcon className="customIcon" height="22" viewBox="0 0 22 22" width="22"/>
-  }
-};
-
-const buildName = (name: {lastName: string, firstName: string}, isFetus: boolean, onNavigateToMotherFile: () => void) => {
-  if (isFetus) {
-    return (
-      <span>
-        { `${intl.get('screen.patient.details.fetus')} ` }
-        <span className="patient-page__header__name__mother-name">
-          (
-          <Button className="link--underline" onClick={() => onNavigateToMotherFile()} type="link">
-            { `${name.lastName.toUpperCase()} ${name.firstName}` }
-          </Button>
-          )
-        </span>
-
-      </span>
-    );
-  }
-
-  return <span>{ `${name.lastName.toUpperCase()} ${name.firstName}` }</span>;
-};
 
 interface Props {
   patient: ParsedPatientData
 }
 
-const PatientHeader: React.FC<Props> = ({ patient }) => {
-  const dispatch = useDispatch();
-
-  return (
-    <div className="header__content--static">
-      <Row align="bottom" className="flex-row patient-header" gutter={12}>
-        <Col>
-          <Typography.Title className="patient-page__header__name" level={3}>
-            { buildName(
-              { firstName: patient.firstName, lastName: patient.lastName },
-              patient.isFetus,
-              () => dispatch(navigateToPatientScreen(patient.familyRelation)),
-            ) }
-          </Typography.Title>
-        </Col>
-        { !patient.isFetus && (
-          <>
-            <Col>
-              { getGenderIcon(patient.gender) }
-            </Col>
-            {
-              patient.birthDate && (
-                <Col>
-                  <Tag className="patient-page__header__tags">
-                    { patient.birthDate }
-                  </Tag>
-                </Col>
-              )
-            }
-          </>
-        ) }
-        <Col>
-          <Tag
-            className="patient-page__header__tags"
-            color={patient.proband === 'Parent' ? 'geekblue' : 'red'}
-          >
-            { patient.proband }
-          </Tag>
-        </Col>
-        { patient.isFetus && (
-          <Col>
-            <Tag
-              className="patient-page__header__tags"
-              color="purple"
-            >
-              { intl.get('screen.patient.details.fetus') }
-            </Tag>
-          </Col>
-        ) }
-      </Row>
-    </div>
-  );
-};
+const PatientHeader= ({ patient }:Props):React.ReactElement => (
+  <div className="header__content--static">
+    <Row align="bottom" className="flex-row patient-header" gutter={12}>
+      { !patient.isFetus && (
+        <ProbandHeader patient={patient}/>
+      )
+      }
+      { patient.isFetus && (
+        <FetusHeader patient={patient} />
+      ) }
+    </Row>
+  </div>
+);
 
 export default PatientHeader;
