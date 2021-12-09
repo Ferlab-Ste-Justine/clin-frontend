0c3db0bce43ed40664067d2970103472122651a9
claudia1296 <claudia.roy.cr@gmail.com>
2021-12-09 15:17:22 -0500

    deplacer fonction + fix retour

--- client/src/helpers/ServiceRequestCode.ts
+++ client/src/helpers/ServiceRequestCode.ts
@@ -1,3 +1,8 @@
 import { Concept } from "store/ServiceRequestCodeTypes";
 
-export const findLocalDesignationIfExists = (concept: Concept | undefined, lang: 'fr' | 'en'): string | undefined =>concept?.designation.find(d => d.language === lang)?.value || undefined
+export const findLocalDesignationIfExists = (concept: Concept | undefined, lang: 'fr' | 'en'): string | null =>concept?.designation?.find(d => d.language === lang)?.value || null
+
+export const translateAnalysis = (testName: string, concepts: Concept[], lang:'fr' | 'en' ): string => {
+  const concept = concepts.find((c)=> c.display === testName)
+  return findLocalDesignationIfExists(concept, lang) || '--'
+}
