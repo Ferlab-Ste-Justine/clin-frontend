3042cad0394f770c7d2de13eae9748bf1dc092bd
Karine St-Onge <karinestonge@CRHSJ106508.local>
2022-02-17 16:02:49 -0500

    CLIN-802 Ajout de tests unitaires pour la classe ObservationBuilder

---
+++ client/src/helpers/fhir/__tests/ObservationBuilder.mocks.ts
@@ -0,0 +1,202 @@
+export const observationCGHMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "laboratory",
+            "display": "Laboratory",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "CGH",
+            "display": "cgh",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationINDICMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "exam",
+            "display": "Exam",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "INDIC",
+            "display": "indications",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationHPOMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "exam",
+            "display": "Exam",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "PHENO",
+            "display": "phenotype",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationINVESMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "exam",
+            "display": "Exam",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "INVES",
+            "display": "investigations",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationETHMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "exam",
+            "display": "Exam",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "ETH",
+            "display": "ethnicity",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationCONSMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "exam",
+            "display": "Exam",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "CONS",
+            "display": "consangunity",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": undefined,
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "final",
+    "subject": undefined,
+    "valueBoolean": undefined,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
+
+  export const observationUpdateMock = {
+    "resourceType": "Observation",
+    "category": [ {
+        "coding": [ {
+            "code": "laboratory",
+            "display": "Laboratory",
+            "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+            } ],
+        } ],
+    "code": {
+        "coding": [ {
+            "code": "CGH",
+            "display": "cgh",
+            "system": "http://fhir.cqgc.ferlab.bio/CodeSystem/observation-code",
+            } ],
+        },
+    "id": "T123",
+    "interpretation": undefined,
+    "meta": {
+        "profile": [ "http://fhir.cqgc.ferlab.bio/StructureDefinition/cqgc-observation" ],
+        },
+    "note": undefined,
+    "status": "amended",
+    "subject": undefined,
+    "valueBoolean": true,
+    "valueCodeableConcept": undefined,
+    "valueString": undefined,
+  }
